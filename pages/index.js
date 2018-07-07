ebc416921d7db13940bdedb56be33e60e42c247a
Mathieu <mathieu@woorank.com>
2018-07-07 11:54:33 +0200

    Dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 const Index = () => (
   <Layout>
     <p>
-      Hello Next!
+      A blog
     </p>
+    <ul>
+      <PostLink title='Hello Next.js' />
+      <PostLink title='Learn Next.js is awesome' />
+      <PostLink title='Deploy apps with Zeit' />
+    </ul>
   </Layout>
 );
 
