88dfa9c61c0a83a8ee120c59d0af97fbd0c85976
Mathieu <mathieu@woorank.com>
2018-07-07 10:36:52 +0200

    Routing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-const Index = () => {
-  return (
-    <div>
-      <p>
-        Hello Next!
-    </div>
-  );
-};
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <p>
+      Hello Next!
+    </p>
+    <Link href='/about'>
+      <a style={{ color: 'red' }}>
+        About
+      </a>
+    </Link>
+    <Link href='/about'>
+      <button style={{ color: 'red' }}>
+        About button
+      </button>
+    </Link>
+  </div>
+);
 
 export default Index;
